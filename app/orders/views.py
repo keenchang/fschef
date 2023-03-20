0ef43fa16fa7cdce623e8a6444c27c057dd59371
keenchang <s1277865@gmail.com>
2023-03-20 18:17:12 +0800

    串line pay金流API

--- app/orders/views.py
+++ app/orders/views.py
@@ -5,14 +5,15 @@ from . import orders_bp
 from app import parameters
 from app.extensions import db, socketio
 from app.models.orders import Order
+from app.models.stores import Store
 from app.models.tables import Table, TableState
-from app.utils import check_login_in
+from app.utils import check_login_in, get_auth_signature
 
 # line pay api的headers參數
-# channel_id = os.environ.get('CHANNEL_ID')
-# channel_secret = os.environ.get('CHANNEL_SECRET')
-# uri = "/v3/payments/request"
-# nonce = str(uuid.uuid4())
+channel_id = os.environ.get('CHANNEL_ID')
+channel_secret = os.environ.get('CHANNEL_SECRET')
+uri = "/v3/payments/request"
+nonce = str(uuid.uuid4())
 
 # 藍新金流api的參數
 # mpg_merchant_id = os.environ.get('MPG_MERCHANT_ID')
@@ -91,79 +92,78 @@ def cancel():
         return jsonify({'message': str(e)}), 500
 
 
-# @orders_bp.route('/order/<int:order_id>/pay')
-# @check_login_in()
-# def pay(order_id):
-#     order = Order.query.get(order_id)
-#     table = Table.query.get(order.table_id)
+@orders_bp.route('/order/<int:order_id>/pay')
+def pay(order_id):
+    order = Order.query.get(order_id)
+    table = Table.query.get(order.table_id)
+    store = Store.query.get(table.store_id)
 
-#     records = order.record
-#     id_list, num_list = '', []
-#     for record in records:
-#         num_list.append(int(record[1]))
-#         id_list += record[0] + ','
-#     id_list = id_list[:-1] 
+    records = order.record
+    id_list, num_list = '', []
+    for record in records:
+        num_list.append(int(record[1]))
+        id_list += record[0] + ','
+    id_list = id_list[:-1] 
 
-#     sql_cmd = f"SELECT name, price FROM menu5 WHERE id IN (" + id_list + ")"
-#     menu_infos = db.engine.execute(sql_cmd).fetchall()
-#     menu_infos = np.array(menu_infos).T.tolist()
+    sql_cmd = f"SELECT name, price FROM menu{store.user_id} WHERE id IN (" + id_list + ")"
+    menu_infos = db.engine.execute(sql_cmd).fetchall()
+    menu_infos = np.array(menu_infos).T.tolist()
 
-#     name_list = menu_infos[0]
-#     price_list = menu_infos[1]
-#     price_list = [int(x) for x in price_list]
+    name_list = menu_infos[0]
+    price_list = menu_infos[1]
+    price_list = [int(x) for x in price_list]
 
-#     amount = np.sum(np.array(price_list) * np.array(num_list))
-#     products_name = "餐點"
+    amount = np.sum(np.array(price_list) * np.array(num_list))
+    products_name = "餐點"
 
-#     product_list = []
-#     for idx, num in enumerate(num_list):
-#         item = {"name": name_list[idx]}
-#         item["quantity"] = num
-#         item["price"] = price_list[idx]
+    product_list = []
+    for idx, num in enumerate(num_list):
+        item = {"name": name_list[idx]}
+        item["quantity"] = num
+        item["price"] = price_list[idx]
 
-#         product_list.append(item)
+        product_list.append(item)
 
-#     url = os.environ.get('URL') + "/order/{order_id}/checkout".format(order_id = order.id)
-#     form_data = {
-#         "amount": str(amount),
-#         "currency": 'TWD',
-#         "orderId": str(order.id),
-#         "packages": [
-#             {
-#                 "id": str(order.id),
-#                 "amount": str(amount),
-#                 "name": products_name,
-#                 "products": product_list
-#             }
-#         ],
-#         "redirectUrls": {
-#             "confirmUrl": url,
-#             "cancelUrl": url
-#         }
-#     }
-#     json_body = json.dumps(form_data)
+    url = os.environ.get('URL') + "/order/{order_id}/checkout".format(order_id = order.id)
+    form_data = {
+        "amount": str(amount),
+        "currency": 'TWD',
+        "orderId": str(order.id),
+        "packages": [
+            {
+                "id": str(order.id),
+                "amount": str(amount),
+                "name": products_name,
+                "products": product_list
+            }
+        ],
+        "redirectUrls": {
+            "confirmUrl": url,
+            "cancelUrl": url
+        }
+    }
+    json_body = json.dumps(form_data)
 
-#     headers = {
-#         'Content-Type': 'application/json',
-#         'X-LINE-ChannelId': channel_id,
-#         'X-LINE-Authorization-Nonce': nonce,
-#         'X-LINE-Authorization': get_auth_signature(channel_secret, uri, json_body, nonce) 
-#     }
+    headers = {
+        'Content-Type': 'application/json',
+        'X-LINE-ChannelId': channel_id,
+        'X-LINE-Authorization-Nonce': nonce,
+        'X-LINE-Authorization': get_auth_signature(channel_secret, uri, json_body, nonce) 
+    }
 
-#     response = requests.post("https://sandbox-api-pay.line.me" + uri, headers=headers, data=json_body)
-#     dict_response = json.loads(response.text)
+    response = requests.post("https://sandbox-api-pay.line.me" + uri, headers=headers, data=json_body)
+    dict_response = json.loads(response.text)
 
-#     if dict_response.get('returnCode') == "0000":
-#         info = dict_response.get('info')
-#         web_url = info.get('paymentUrl').get('web')
+    if dict_response.get('returnCode') == "0000":
+        info = dict_response.get('info')
+        web_url = info.get('paymentUrl').get('web')
 
-#         return redirect(web_url)
-#     else:
-#         return redirect(url_for('menus', store_id=table.store_id, table_id=table.id))
+        return redirect(web_url)
+    else:
+        return redirect(url_for('menus', store_id=table.store_id, table_id=table.id))
 
 
 # @orders_bp.route('/order/<int:order_id>/blue_pay')
-# @check_login_in()
 # def blue_pay(order_id):
 #     order = Order.query.get(order_id)
 
@@ -198,17 +198,16 @@ def cancel():
 #     return render_template("orders/newebpay.html", form_info=form_info)
 
 
-# @orders_bp.route('/order/<int:order_id>/checkout')
-# def checkout(order_id):
-#     order = Order.query.get(order_id)
-#     table = Table.query.get(order.table_id)
+@orders_bp.route('/order/<int:order_id>/checkout')
+def checkout(order_id):
+    order = Order.query.get(order_id)
+    table = Table.query.get(order.table_id)
 
-#     table.status = TableState.PAY
-#     db.session.commit()
+    table.status = TableState.PAY
+    db.session.commit()
 
-#     message = { "storeId": table.store_id, "tableId": table.id, "tableStatus": table.status.value }
-#     socketio.emit("sendOrderStatus", message, broadcast=True)
+    message = { "storeId": table.store_id, "tableId": table.id, "tableStatus": table.status.value }
+    socketio.emit("sendOrderStatus", message, broadcast=True)
 
-#     return redirect(url_for('menus', store_id=table.store_id, table_id=table.id))
-
+    return redirect(url_for('customers_bp.menus', store_id=table.store_id, table_id=table.id))
 
--- app/utils.py
+++ app/utils.py
@@ -1,3 +1,4 @@
+import hashlib, hmac, base64, time
 from flask import session, redirect, url_for
 
 
@@ -16,3 +17,19 @@ def check_login_in():
         return wrap
  
     return decorator
+
+
+# line pay的API加密函數
+def get_auth_signature (secret, uri, body, nonce):
+    """
+    用於製作密鑰
+    :param secret: your channel secret
+    :param uri: uri
+    :param body: request body
+    :param nonce: uuid or timestamp(時間戳)
+    :return:
+    """
+    str_sign = secret + uri + body + nonce
+    return base64.b64encode(hmac.new(str.encode(secret), str.encode(str_sign), digestmod=hashlib.sha256).digest()).decode("utf-8")
+
+
